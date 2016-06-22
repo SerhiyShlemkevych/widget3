f538495142239e61ecc8127b6ed024f66cd9ee94
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-23 00:05:21 +0300

    Class for providing common create steps

---
+++ widget3/Code/CommonCreateTileSteps.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using widget3.ViewModels.Abstract.Common;
+using widget3.Views.Settings.CreateTilePages;
+
+namespace widget3.Code
+{
+    public class CommonCreateTileSteps
+    {
+        public static CreateTileStep BackgroundStep
+        {
+            get
+            {
+                return new CreateTileStep()
+                {
+                    PageHeader = "Select background",
+                    Page = new SelectBackgroundPage(),
+                    Validate = (tile) => tile != null && tile.Background != null
+                };
+            }
+        }
+
+        public static CreateTileStep TextStep
+        {
+            get
+            {
+                return new CreateTileStep()
+                {
+                    PageHeader = "Enter desired text",
+                    Page = new SelectTextPage(),
+                    Validate = (tile) => tile != null && tile.Data !=null
+                };
+            }
+        }
+
+        public static CreateTileStep SizeStep
+        {
+            get
+            {
+                return new CreateTileStep()
+                {
+                    PageHeader = "Select size",
+                    Page = new SelectSizePage(),
+                    Validate = (tile) => tile != null && tile.Width != 0 && tile.Height != 0
+                };
+            }
+        }
+
+        public static CreateTileStep DaysStep
+        {
+            get
+            {
+                return new CreateTileStep()
+                {
+                    PageHeader = "Select days for tile",
+                    Page = new SelectDaysPage(),
+                    Validate = (tile) => tile != null && tile.Days.Any(d => d)
+                };
+            }
+        }
+
+    }
+}
