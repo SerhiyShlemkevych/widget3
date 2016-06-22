610919735f56fce1c51f50ca84145653d2adad7e
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-23 00:06:02 +0300

    Viewmodel for common settings page

---
+++ widget3/ViewModels/Concrete/SettingsWindow/CommonSettingsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using widget3.Services.Abstract;
+using widget3.ViewModels.Abstract.SettingsWindow;
+using widget3.ViewModels.Concrete.Common;
+using widget3.Views.Settings;
+
+namespace widget3.ViewModels.Concrete.SettingsWindow
+{
+    public class CommonSettingsViewModel : ChildViewModel
+    {
+        public CommonSettingsViewModel(ParentViewModel parent, IUserDataService userData) : base(parent, userData)
+        {
+            Name = "Common settings";
+            Page = new CommonSettingsPage() { DataContext = this };
+        }
+    }
+}
