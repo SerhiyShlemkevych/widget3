87a84f9f63d0b9d7499bf262440bf2073690444a
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-23 00:01:29 +0300

    Base class for tile views

--- widget3/Controls/Abstract/Common/TileBase.cs
+++ widget3/Controls/Abstract/Common/TileBase.cs
@@ -6,16 +6,52 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace widget3.Controls.Abstract.Common
 {
-    public class TileBase : UserControl
+    public class TileBase : UserControl, ICommandSource
     {
+        public TileBase()
+        {
+            MouseUp += TileBaseMouseUp;
+            SetResourceReference(TileBase.BorderBrushProperty, "SelectedBorderColor");
+            Foreground = Brushes.White;
+        }
+
+        private void TileBaseMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            var command = Command;
+            var parameter = CommandParameter;
+            var target = CommandTarget;
+
+            var routedCommand = command as RoutedCommand;
+            if (routedCommand != null && routedCommand.CanExecute(parameter, target))
+            {
+                routedCommand.Execute(parameter, target);
+            }
+            else if (command != null && command.CanExecute(parameter))
+            {
+                command.Execute(parameter);
+            }
+        }
+
+
+        public static readonly DependencyProperty CommandTargetProperty =
+            DependencyProperty.Register("CommandTarget", typeof(IInputElement), typeof(TileBase), new UIPropertyMetadata(null));
+
+
         public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(TileBase));
         public static readonly DependencyProperty CommandProperty = DependencyProperty.Register("Command", typeof(ICommand), typeof(TileBase));
         public static readonly DependencyProperty CommandParameterProperty = DependencyProperty.Register("CommandParameter", typeof(object), typeof(TileBase));
 
         public static readonly DependencyProperty SubFontSizeProperty = DependencyProperty.Register("SubFontSize", typeof(int), typeof(TileBase));
+
+        public IInputElement CommandTarget
+        {
+            get { return (IInputElement)GetValue(CommandTargetProperty); }
+            set { SetValue(CommandTargetProperty, value); }
+        }
 
         public string Text
         {
