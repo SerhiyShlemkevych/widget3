48da3bdd1e151acd59c0cd5ab83433df4b512f5a
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-23 00:08:21 +0300

    Favorite backgrounds page

---
+++ widget3/Views/Settings/FavoriteBackgroundsPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Gat.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace widget3.Views.Settings
+{
+    /// <summary>
+    /// Interaction logic for FavoriteBackgroundsPage.xaml
+    /// </summary>
+    public partial class FavoriteBackgroundsPage : Page
+    {
+        public FavoriteBackgroundsPage()
+        {
+            InitializeComponent();            
+        }
+    }
+}
