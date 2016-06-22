8ce0f575d0a21d1fec7ff083bb92616814906d99
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-23 00:02:12 +0300

    Class for displaying editing UI

---
+++ widget3/Code/TileEditInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace widget3.Code
+{
+    public class TileEditPropertyInfo
+    {
+        public string Label
+        {
+            get;
+            set;
+        }
+
+        public Control Control
+        {
+            get;
+            set;
+        }
+    }
+}
