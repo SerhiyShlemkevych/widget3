275573c06dd2b6b0da3c7941b2a964b1d1b28c38
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-23 00:07:41 +0300

    ICommand that uses delegate

---
+++ widget3/Commands/Abstract/DelegateCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace widget3.Commands.Abstract
+{
+    public class DelegateCommand : ICommand
+    {
+        private Action _action;
+
+        private Func<bool> _check;
+
+        public DelegateCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public DelegateCommand(Action action, Func<bool> actionCanExecute)
+        {
+            _action = action;
+            _check = actionCanExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (_check == null)
+            {
+                return true;
+            }
+
+            return _check();
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_action == null)
+            {
+                throw new ArgumentNullException("action");
+            }
+
+            _action();
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ widget3/Commands/Abstract/DelegateCommandGeneric.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace widget3.Commands.Abstract
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        private Action<T> _action;
+
+        private Func<T, bool> _check;
+
+        public DelegateCommand(Action<T> action)
+        {
+            _action = action;
+        }
+
+        public DelegateCommand(Action<T> action, Func<T, bool> actionCanExecute)
+        {
+            _action = action;
+            _check = actionCanExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (_check == null)
+            {
+                return true;
+            }
+
+            return _check((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_action == null)
+            {
+                throw new ArgumentNullException("action");
+            }
+
+            _action((T)parameter);
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            if(CanExecuteChanged!= null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
