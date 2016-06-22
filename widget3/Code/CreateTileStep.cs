c80f55c4535ccb5fa0d4cc42c3cd25529dc67599
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-23 00:06:36 +0300

    Class that represents step of creation of tile

---
+++ widget3/Code/CreateTileStep.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using widget3.ViewModels.Abstract.Common;
+
+namespace widget3.Code
+{
+    public class CreateTileStep
+    {
+        public string PageHeader
+        {
+            get;
+            set;
+        }
+
+        public Page Page
+        {
+            get;
+            set;
+        }
+
+        public Func<TileViewModel, bool> Validate
+        {
+            get;
+            set;
+        }
+    }
+}
