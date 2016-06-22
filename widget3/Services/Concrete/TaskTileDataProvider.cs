9f1cb0d60b70d70a0f5866537ba52ceac58b64d5
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-23 00:12:24 +0300

    Data provider for task tile

---
+++ widget3/Services/Concrete/TaskTileDataProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using widget3.Controls.Concrete.MainWindow;
+using widget3.Services.Abstract;
+using widget3.ViewModels.Concrete.Common;
+
+namespace widget3.Services.Concrete
+{
+    public class TaskTileDataProvider : TileDataProvider<TaskTileViewModel>
+    {
+        public TaskTileDataProvider(IUserDataService userData) : base(userData)
+        {
+        }
+
+        protected override void ProvideTileWithValue(TaskTileViewModel tile)
+        {
+            tile.Text = (string)tile.Data;
+        }
+    }
+}
