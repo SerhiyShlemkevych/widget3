74272e6421247add1b3b2d7ef69311210f572e93
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-23 00:11:33 +0300

    Improved startup logic

--- widget3/App.xaml.cs
+++ widget3/App.xaml.cs
@@ -5,9 +5,11 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using widget3.Converters;
 using widget3.Services.Abstract;
 using widget3.Services.Concrete;
 using widget3.ViewModels.Concrete.MainWindow;
+using widget3.ViewModels.Concrete.SettingsWindow;
 
 namespace widget3
 {
@@ -17,6 +19,8 @@ namespace widget3
     public partial class App : Application
     {
         private MainWindowViewModel _mainWindowViewModel;
+        private SettingsWindowViewModel _settingsWindowViewModel;
+        private List<ITileDataProvider> _tileDataProviders;
 
         public App()
         {
@@ -25,6 +29,15 @@ namespace widget3
             userData.Load();
 
             _mainWindowViewModel = new MainWindowViewModel(userData);
+            _settingsWindowViewModel = new SettingsWindowViewModel(userData);
+            InitializeTileDataProviders(userData);
+        }
+
+        private void InitializeTileDataProviders(IUserDataService userData)
+        {
+            _tileDataProviders = new List<ITileDataProvider>();
+
+            _tileDataProviders.Add(new TaskTileDataProvider(userData));
         }
     }
 }
