5aa7581b2d584d5f3ea31fe293307d861d266079
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-23 00:11:09 +0300

    Base class for parental viewmodels

---
+++ widget3/ViewModels/Abstract/SettingsWindow/ParentViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using widget3.ViewModels.Abstract.Common;
+
+namespace widget3.ViewModels.Abstract.SettingsWindow
+{
+    public abstract class ParentViewModel : ViewModel
+    {
+        private ChildViewModel _activeViewModel;
+
+        public ChildViewModel ActiveViewModel
+        {
+            get
+            {
+                return _activeViewModel;
+            }
+            private set
+            {
+                _activeViewModel = value;
+                OnPropertyChanged("ActiveViewModel");
+            }
+        }
+
+        public void ActivateViewModel(ChildViewModel viewModel)
+        {
+            ActiveViewModel?.Deactivate();
+            ActiveViewModel = viewModel;
+            ActiveViewModel?.Activate();
+        }
+    }
+}
