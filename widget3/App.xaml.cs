554978608976429287a8da814d0b3a8bac40cba3
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-26 17:27:42 +0300

    Added new tile data providers

--- widget3/App.xaml.cs
+++ widget3/App.xaml.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Xml;
+using System.Xml.Serialization;
+using widget3.Code;
 using widget3.Converters;
+using widget3.Models;
 using widget3.Services.Abstract;
 using widget3.Services.Concrete;
 using widget3.ViewModels.Concrete.MainWindow;
@@ -38,6 +44,8 @@ namespace widget3
             _tileDataProviders = new List<ITileDataProvider>();
 
             _tileDataProviders.Add(new TaskTileDataProvider(userData));
+            _tileDataProviders.Add(new AlarmTileDataProvider(userData));
+            _tileDataProviders.Add(new WeatherTileDataProvider(userData));
         }
     }
 }
