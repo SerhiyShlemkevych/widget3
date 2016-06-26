b3e17eca4f981f6574265e33fd012c72d467af76
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-26 17:20:54 +0300

    Weather tile view

--- widget3/Controls/Concrete/MainWindow/WeatherTile.xaml.cs
+++ widget3/Controls/Concrete/MainWindow/WeatherTile.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using widget3.Controls.Abstract.Common;
+using widget3.Converters;
 
 namespace widget3.Controls.Concrete.MainWindow
 {
