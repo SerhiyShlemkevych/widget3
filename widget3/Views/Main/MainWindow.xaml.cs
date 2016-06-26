8640286bc28fa0c2446b7cf43ada0ac8c50b6bb3
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-26 17:25:24 +0300

    Removed temporary buttons

--- widget3/Views/Main/MainWindow.xaml.cs
+++ widget3/Views/Main/MainWindow.xaml.cs
@@ -17,11 +17,32 @@ namespace widget3.Views.Main
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+
+
     public partial class MainWindow : Window
     {
+        private Window _fakeWindow;
+
         public MainWindow()
         {
             InitializeComponent();
+            HideIcon();
+        }
+
+        private void HideIcon() //hide from alt+tab page
+        {
+            _fakeWindow = new Window();
+            _fakeWindow.Top = -100;
+            _fakeWindow.Left = -100;
+            _fakeWindow.Width = 1;
+            _fakeWindow.Height = 1;
+            _fakeWindow.WindowStyle = WindowStyle.ToolWindow;
+            _fakeWindow.ShowInTaskbar = false;
+            _fakeWindow.Show();
+            this.Owner = _fakeWindow;
+            _fakeWindow.Hide();
         }
     }
 }
