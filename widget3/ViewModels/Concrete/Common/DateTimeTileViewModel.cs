1c18c659d0a8236f90a27da8ea4ba5249cba8678
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-28 23:20:18 +0300

    Datetime tile view model

---
+++ widget3/ViewModels/Concrete/Common/DateTimeTileViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using widget3.Code;
+using widget3.Controls.Abstract.Common;
+using widget3.Controls.Concrete.MainWindow;
+using widget3.Enums;
+using widget3.ViewModels.Abstract.Common;
+
+namespace widget3.ViewModels.Concrete.Common
+{
+    public class DateTimeTileViewModel : TileViewModel
+    {
+        private string _date;
+        private string _day;
+
+        public DateTimeTileViewModel()
+        {
+            Type = TileType.DateTime;
+        }
+
+        public string Date
+        {
+            get
+            {
+                return _date;
+            }
+            set
+            {
+                _date = value;
+                OnPropertyChanged("Date");
+            }
+        }
+
+
+        public string Day
+        {
+            get
+            {
+                return _day;
+            }
+            set
+            {
+                _day = value;
+                OnPropertyChanged("Day");
+            }
+        }
+
+
+
+        public override TileBase CreateTileView()
+        {
+            var tileView = new DateTimeTile() { DataContext = this };
+            SetBindings(tileView);
+            return tileView;
+        }
+
+        public override IEnumerable<CreateTileStep> GetCreateSteps()
+        {
+            return new List<CreateTileStep>()
+            {
+                CommonCreateTileSteps.BackgroundStep,
+                CommonCreateTileSteps.SizeStep,
+                CommonCreateTileSteps.DaysStep
+            };
+        }
+
+        public override IEnumerable<TileEditPropertyInfo> GetEditInfo()
+        {
+            return new List<TileEditPropertyInfo>()
+            {
+                CommonEditPropertyInfos.Background,
+                CommonEditPropertyInfos.Transperency,
+                CommonEditPropertyInfos.Width,
+                CommonEditPropertyInfos.Height,
+                CommonEditPropertyInfos.Days
+            };
+        }
+
+        public override void SetDefaultCommand()
+        {
+            
+        }
+    }
+}
