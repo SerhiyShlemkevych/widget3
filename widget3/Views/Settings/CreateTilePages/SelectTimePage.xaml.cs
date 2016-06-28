378ef75e7f1a8f90ee25903e08a2f7dd5ece157f
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-28 09:31:03 +0300

    CReate tile select time page

---
+++ widget3/Views/Settings/CreateTilePages/SelectTimePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using widget3.Converters;
+
+namespace widget3.Views.Settings.CreateTilePages
+{
+    /// <summary>
+    /// Interaction logic for SelectTimePage.xaml
+    /// </summary>
+    public partial class SelectTimePage : Page
+    {
+        public SelectTimePage()
+        {
+            Resources.Add("SmallTimeConverter", new SmallTimeConverter());
+            InitializeComponent();
+        }
+    }
+}
