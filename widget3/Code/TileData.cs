5d84cec68378d59dbb5a8281d5b32f61995ea446
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-28 09:33:43 +0300

    Prent class for tile data

---
+++ widget3/Code/TileData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace widget3.Code
+{
+    public class TileData
+    {
+        public string Text
+        {
+            get;
+            set;
+        }
+    }
+}
