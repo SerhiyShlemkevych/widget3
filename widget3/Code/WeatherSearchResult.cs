c3522ba6279840cf41229eca74031da8d596030f
Shlemkevych Serhiy <shlemkevych.s@gmail.com>
2016-06-28 09:33:25 +0300

    Search weather locaton result

---
+++ widget3/Code/WeatherSearchResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace widget3.Code
+{
+    public class WeatherSearchResult : TileData
+    {
+        public string City
+        {
+            get;
+            set;
+        }
+
+        public string Country
+        {
+            get;
+            set;
+        }
+
+        public string Key
+        {
+            get;
+            set;
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0}, {1}", City, Country);
+            }
+        }
+    }
+}
